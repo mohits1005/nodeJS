abcaa5a01705b641f842555ed7a66716f33cfbbf
mohit sharma <mohit.shar1005@gmail.com>
2018-05-14 01:18:00 +0530

    Code cleaning

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,39 +1,6 @@
 console.log('Starting app.js');
 
 const fs = require('fs');
-
-const os = require('os');
-
-const notes = require('./notes.js');
-
 const _ = require('lodash');
 
-// console.log(_.isString(true));
-// console.log(_.isString('Mohit'));
-
-var filteredArray = _.uniq(['Mohit',1,'Mohit',1,2,3,4])
-console.log(filteredArray)
-
-// var res = notes.addNote();
-//
-// console.log(res);
-//
-// var res2 = notes.addFunc(2,3);
-//
-// console.log(res2);
-
-// var user = os.userInfo();
-
-// console.log(user);
-
-// fs.appendFile('greetings.txt','Hello '+user.username,function(err){
-//     if(err){
-//         console.log('Unable to write to file');
-//     }
-// });
-
-// fs.appendFile('greetings.txt',`Hello ${user.username} ! You are ${notes.age}`,function(err){
-//     if(err){
-//         console.log('Unable to write to file');
-//     }
-// });
+const notes = require('./notes.js');
--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,15 +1,6 @@
 console.log('Starting node.js')
 
-// console.log(module)
-
-// module.exports.age = 25
-
 module.exports.addNote = () => {
     console.log('addNote');
     return 'New note';
-}
-
-module.exports.addFunc = (a,b) => {
-    console.log('addFunc');
-    return a+b;
 }
