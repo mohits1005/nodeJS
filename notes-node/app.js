e6e843102da5d9ca07fcefd259ae53603b87a597
mohit sharma <mohit.shar1005@gmail.com>
2018-05-15 00:05:44 +0530

    yargs to format input from command line

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,22 +2,27 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const _ = require('lodash');
+const yargs = require('yargs')
 
 const notes = require('./notes.js');
 
+const argv = yargs.argv;
 var command = process.argv[2];
 console.log('Command: ',command);
-
-console.log(process.argv);
+console.log('Yargs: ',argv);
 
 if(command === 'add'){
-    console.log('Adding new note');
+    // console.log('Adding new note');
+    notes.addNote(argv.title, argv.body);
 }else if(command === 'list'){
-    console.log('Listing all notes');
+    // console.log('Listing all notes');
+    notes.getAll();
 }else if(command === 'read'){
-    console.log('Reading note');
+    // console.log('Reading note');
+    notes.getNote(argv.title);
 }else if(command === 'remove'){
-    console.log('Removing note');
+    // console.log('Removing note');
+    notes.removeNote(argv.title);
 }else{
     console.log('Command not recognized');
 }
--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,6 +1,25 @@
 console.log('Starting node.js')
 
-module.exports.addNote = () => {
-    console.log('addNote');
-    return 'New note';
+var addNote = (title,body) => {
+    console.log('Adding note', title, body);
 }
+
+var getAll = () => {
+    console.log('Getting all notes');
+}
+
+var getNote = (title) => {
+    console.log('Getting note',title);
+}
+
+var removeNote = (title) => {
+    console.log('Removing note',title);
+}
+
+module.exports = {
+    // addNote: addNote
+    addNote,
+    getAll,
+    getNote,
+    removeNote
+};
