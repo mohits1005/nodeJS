3ec18698657bb03a7f218e548fb77a5076e3b021
mohit sharma <mohit.shar1005@gmail.com>
2018-05-18 00:54:09 +0530

    adding notes

--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,7 +1,37 @@
-console.log('Starting node.js')
+console.log('Starting node.js');
+
+const fs = require("fs");
+
 
 var addNote = (title,body) => {
-    console.log('Adding note', title, body);
+    // console.log('Adding note', title, body);
+    var notes = [];
+    var note = {
+        title,
+        body
+    };
+
+    try
+    {
+        var notesString = fs.readFileSync('notes-data.json');
+        notes = JSON.parse(notesString);
+    }
+    catch(e)
+    {
+
+    }
+
+    // var duplicateNotes = notes.filter((note) => {
+    //     return note.title === title;
+    // });
+    var duplicateNotes = notes.filter((note) => note.title === title);
+    if(duplicateNotes.length === 0)
+    {
+        notes.push(note);
+        fs.writeFileSync('notes-data.json',JSON.stringify(notes));
+    }
+
+
 }
 
 var getAll = () => {
