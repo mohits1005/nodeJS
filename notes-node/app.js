245d56fe2cd00406a28e7235a7564c5f7eaea9cf
mohit sharma <mohit.shar1005@gmail.com>
2018-05-22 01:14:03 +0530

    Yargs methods input arguments

--- notes-node/app.js
+++ notes-node/app.js
@@ -4,7 +4,35 @@ const yargs = require('yargs')
 
 const notes = require('./notes.js');
 
-const argv = yargs.argv;
+const titleOptions = {
+    describe:'Title of Note',
+    demand: true,
+    alias: 't'
+};
+
+const bodyOptions = {
+    describe:'Body of Note',
+    demand: true,
+    alias: 'b'
+};
+
+const argv = yargs
+    .command('add','Add a new note',{
+        title:titleOptions,
+        body:bodyOptions
+    })
+    .command('list','List all notes')
+    .command('read','Read a note',{
+        title:titleOptions
+    })
+    .command('read','Read a note',{
+        title:titleOptions
+    })
+    .command('remove','Remove a note',{
+        title:titleOptions
+    })
+    .help()
+    .argv;
 var command = process.argv[2];
 // console.log('Command: ',command);
 // console.log('Yargs: ',argv);
