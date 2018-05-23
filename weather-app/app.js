69f48f067b8c85b2f4e83f69a5e825bae5597e84
mohit sharma <mohit.shar1005@gmail.com>
2018-05-24 01:32:05 +0530

    Printing latitude and longitude from google api json request

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,5 +4,8 @@ request({
     url:"https://maps.googleapis.com/maps/api/geocode/json?address=221b%20baker%20street%20london",
     json: true
 },(error,response,body)=>{
-    console.log(JSON.stringify(body, undefined, 2));
+    // console.log(JSON.stringify(response, undefined, 2));
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lng}`);
 });
