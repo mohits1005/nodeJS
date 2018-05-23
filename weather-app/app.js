336d315a512c510833a04dbe6af4bba9506207e7
mohit sharma <mohit.shar1005@gmail.com>
2018-05-24 00:40:44 +0530

    callbacks and request to google maps api

---
+++ weather-app/app.js
@@ -0,0 +1,8 @@
+const request = require('request');
+
+request({
+    url:"https://maps.googleapis.com/maps/api/geocode/json?address=221b%20baker%20street%20london",
+    json: true
+},(error,response,body)=>{
+    console.log(body);
+});
