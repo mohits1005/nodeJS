eec70833d39b82b10a9bccc96c42b590c6bc9bd2
mohit sharma <mohit.shar1005@gmail.com>
2018-05-25 02:26:56 +0530

    abstracting geocode function

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,6 +1,8 @@
 const request = require('request');
 const yargs = require('yargs');
 
+const geocode = require('./geocode/geocode');
+
 const argv = yargs.options({
     a:{
         demand: true,
@@ -13,27 +15,12 @@ const argv = yargs.options({
 .alias('help','h')
 .argv;
 
-// console.log(encodeURIComponent(argv['a']));
-var encodedAddress = encodeURIComponent(argv.address);
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+    if(errorMessage){
+        console.log(errorMessage);
+    }else
+    {
+        console.log(JSON.stringify(results, undefined, 2));
+    }
 
-request({
-    // url:"https://maps.googleapis.com/maps/api/geocode/json?address=221b%20baker%20street%20london",
-    url:`https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
-    json: true
-},(error,response,body)=>{
-    if(error)
-    {
-        console.log('Unable to connect to Google servers');
-    }
-    else if(body.status === 'ZERO_RESULTS')
-    {
-        console.log('Unable to find that address.');
-    }
-    else if(body.status === 'OK')
-    {
-        // console.log(JSON.stringify(response, undefined, 2));
-        console.log(`Address: ${body.results[0].formatted_address}`);
-        console.log(`Longitude: ${body.results[0].geometry.location.lat}`);
-        console.log(`Latitude: ${body.results[0].geometry.location.lng}`);
-    }
 });
