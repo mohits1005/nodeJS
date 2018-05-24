16b3306cd0da2e16c6989515e3965e6df9e520ac
mohit sharma <mohit.shar1005@gmail.com>
2018-05-25 01:08:30 +0530

    input address and URI encode

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,7 +1,24 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs.options({
+    a:{
+        demand: true,
+        alias: 'address',
+        describe: 'Address to fetch weather for',
+        string: true
+    }
+})
+.help()
+.alias('help','h')
+.argv;
+
+// console.log(encodeURIComponent(argv['a']));
+var encodedAddress = encodeURIComponent(argv.address);
 
 request({
-    url:"https://maps.googleapis.com/maps/api/geocode/json?address=221b%20baker%20street%20london",
+    // url:"https://maps.googleapis.com/maps/api/geocode/json?address=221b%20baker%20street%20london",
+    url:`https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
     json: true
 },(error,response,body)=>{
     // console.log(JSON.stringify(response, undefined, 2));
