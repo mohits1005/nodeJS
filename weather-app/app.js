5b34afe75d3b544ee884be00fcb7c7ea67914717
mohit sharma <mohit.shar1005@gmail.com>
2018-05-25 01:27:05 +0530

    error handling googleapis

--- weather-app/app.js
+++ weather-app/app.js
@@ -21,8 +21,19 @@ request({
     url:`https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
     json: true
 },(error,response,body)=>{
-    // console.log(JSON.stringify(response, undefined, 2));
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Longitude: ${body.results[0].geometry.location.lat}`);
-    console.log(`Latitude: ${body.results[0].geometry.location.lng}`);
+    if(error)
+    {
+        console.log('Unable to connect to Google servers');
+    }
+    else if(body.status === 'ZERO_RESULTS')
+    {
+        console.log('Unable to find that address.');
+    }
+    else if(body.status === 'OK')
+    {
+        // console.log(JSON.stringify(response, undefined, 2));
+        console.log(`Address: ${body.results[0].formatted_address}`);
+        console.log(`Longitude: ${body.results[0].geometry.location.lat}`);
+        console.log(`Latitude: ${body.results[0].geometry.location.lng}`);
+    }
 });
