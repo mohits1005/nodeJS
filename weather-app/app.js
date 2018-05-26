cbc2556d0c631c83f53052b57c75eac6a171fbda
mohit sharma <mohit.shar1005@gmail.com>
2018-05-27 04:57:36 +0530

    chain weather api with google apis

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,43 +1,41 @@
-// const request = require('request');
-// const yargs = require('yargs');
-//
-// const geocode = require('./geocode/geocode');
-//
-// const argv = yargs.options({
-//     a:{
-//         demand: true,
-//         alias: 'address',
-//         describe: 'Address to fetch weather for',
-//         string: true
-//     }
-// })
-// .help()
-// .alias('help','h')
-// .argv;
-//
-// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-//     if(errorMessage){
-//         console.log(errorMessage);
-//     }else
-//     {
-//         console.log(JSON.stringify(results, undefined, 2));
-//     }
-//
-// });
-
-
-//32d6a17ec4f423716861ab90bc8620f7
 const request = require('request');
-request({
-    url:`https://api.darksky.net/forecast/32d6a17ec4f423716861ab90bc8620f7/77.6445923,12.911862`,
-    json: true
-},(error,response,body)=>{
-    if(!error && response.statusCode === 200)
-    {
-        console.log(body.currently.temperature);
+const yargs = require('yargs');
+
+const geocode = require('./geocode/geocode');
+const weather = require('./weather/weather');
+
+
+const argv = yargs.options({
+    a:{
+        demand: true,
+        alias: 'address',
+        describe: 'Address to fetch weather for',
+        string: true
     }
-    else
+})
+.help()
+.alias('help','h')
+.argv;
+
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+    if(errorMessage){
+        console.log(errorMessage);
+    }else
     {
-        console.log('Unable to fetch weather');
+        // console.log(JSON.stringify(results, undefined, 2));
+        console.log(results.address);
+        Latitude = results.Latitude;
+        Longitude = results.Longitude;
+        weather.getWeather(Latitude,Longitude, (errorMessage, weatherResults) => {
+            if(errorMessage){
+                console.log(errorMessage);
+            }else
+            {
+                // console.log(JSON.stringify(weatherResults, undefined, 2));
+                console.log(`It's currently ${weatherResults.temperature}. It feels like ${weatherResults.apparentTemperature}.`);
+            }
+        });
+
     }
+
 });
---
+++ weather-app/weather/weather.js
@@ -0,0 +1,24 @@
+const request = require('request');
+//32d6a17ec4f423716861ab90bc8620f7
+var getWeather = (lat,lng, callback) => {
+    request({
+        url:`https://api.darksky.net/forecast/32d6a17ec4f423716861ab90bc8620f7/${lat},${lng}`,
+        json: true
+    },(error,response,body)=>{
+        if(!error && response.statusCode === 200)
+        {
+            callback(undefined,{
+                temperature: body.currently.temperature,
+                apparentTemperature: body.currently.apparentTemperature
+            });
+        }
+        else
+        {
+            callback('Unable to fetch weather');
+        }
+    });
+}
+
+module.exports = {
+    getWeather
+};
