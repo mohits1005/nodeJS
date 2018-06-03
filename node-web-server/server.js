3f4c859a267318cd14413bf6e6fe7733618f3f2f
mohit sharma <mohit.shar1005@gmail.com>
2018-06-04 02:01:02 +0530

    express basics - set up routes

---
+++ node-web-server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+var app = express();
+
+app.get('/', (req,res) => {
+    // res.send('<h1>Hello Express!</h1>');
+    res.send({
+        name: 'Mohit',
+        likes: [
+            'Biking',
+            'Cities'
+        ]
+    });
+});
+
+app.get('/about', (req,res) => {
+    res.send('About Page');
+});
+
+//bad - send back json with errorMessage property
+app.get('/bad', (req,res) => {
+    res.send({
+        errorMessage: 'Unable to handle request'
+    });
+});
+
+app.listen(3000);
