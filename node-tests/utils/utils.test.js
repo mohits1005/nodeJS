ba61cfc4e95f87684576032e105d1c44163bb303
mohit sharma <mohit.shar1005@gmail.com>
2018-06-09 03:47:21 +0530

    make assertions using npm expect

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -1,15 +1,34 @@
+const expect = require('expect');
 const utils = require('./utils');
 
 it('should add two numbers', () => {
     var res = utils.add(33, 11);
-    if(res != 44){
-        throw new Error(`Expected 44 , but got ${res}.`);
-    }
+    expect(res).toBe(44).toBeA('number');
 });
 
 it('should square a number', () => {
     var res = utils.square(3);
-    if(res !== 9){
-        throw new Error(`Expected 9 , but got ${res}.`);
-    }
+    expect(res).toBe(9).toBeA('number');
 });
+
+it('should set first name and last name', ()=> {
+    var user = {location:'blr',age:25};
+    var res = utils.setName(user, 'moh vinsmoke');
+    expect(res).toInclude({
+        firstName:'moh',
+        lastName:'vinsmoke'
+    })
+});
+
+// it('should expect some values', ()=> {
+//     // expect(12).toNotBe(11);
+//     // expect({name:'moh'}).toEqual({name:'moh'});
+//     // expect([2,3,4]).toInclude(2);
+//     expect({
+//         name:'moh',
+//         age:25,
+//         location:'blr'
+//     }).toInclude({
+//         age:25
+//     })
+// });
