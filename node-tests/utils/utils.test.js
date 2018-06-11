bea8fb77f722181e72c930b63e3e0cd0e47b7243
mohit sharma <mohit.shar1005@gmail.com>
2018-06-12 01:33:20 +0530

    testing asynchronus functions

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -6,9 +6,23 @@ it('should add two numbers', () => {
     expect(res).toBe(44).toBeA('number');
 });
 
+it('should async add two numbers', (done) => {
+    utils.asyncAdd(4, 3, (sum)=>{
+        expect(sum).toBe(7).toBeA('number');
+        done();
+    });
+});
+
 it('should square a number', () => {
     var res = utils.square(3);
     expect(res).toBe(9).toBeA('number');
+});
+
+it('should async square a number', (done) => {
+    utils.asyncSquare(3, (res)=>{
+        expect(res).toBe(9).toBeA('number');
+        done();
+    });
 });
 
 it('should set first name and last name', ()=> {
