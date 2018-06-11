bf82dccb6531621762d27988fe6d81b43e45f471
mohit sharma <mohit.shar1005@gmail.com>
2018-06-12 03:13:14 +0530

    using npm supertest with mocha

--- node-tests/server/server.js
+++ node-tests/server/server.js
@@ -3,7 +3,23 @@ const express = require('express');
 var app = express();
 
 app.get('/',(req,res) => {
-    res.send('Hello World!');
+    res.status(404).send({
+        error: 'Page not found',
+        name: 'app v1.0'
+    });
+});
+
+app.get('/users',(req,res) => {
+    res.status(200).send([{
+        name: 'moh',
+        age: 25
+    },
+    {
+        name: 'john',
+        age:25
+    }
+    ]);
 });
 
 app.listen(3000);
+module.exports.app = app;
