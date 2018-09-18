1758b7877de05e47bca315c9ffae3bbf6b4071fc
mohit sharma <mohit.shar1005@gmail.com>
2018-09-18 23:31:26 +0530

    seeding database with dummy values

--- mongodb-node/playground/mongodb-connect.js
+++ mongodb-node/playground/mongodb-connect.js
@@ -5,5 +5,30 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp',{ useNewUrlParser: true 
         return console.log('Unable to connect to MongoDB server');
     }
     console.log('Connected to MongoDB server');
+    const dbClient = db.db('Todos');
+
+    // dbClient.collection('Todos').insertOne({
+    //     text: 'Testing Todos app',
+    //     completed: false
+    // }, (err, result) => {
+    //     if(err){
+    //         return console.log('Unable to insert todo', err);
+    //     }
+    //     console.log(JSON.stringify(result.ops, undefined, 2));
+    // });
+
+    // dbClient.collection('Users').insertOne({
+    //     name: 'Robo',
+    //     age: 22,
+    //     location: 'blr'
+    // }, (err, result) => {
+    //     if(err){
+    //         return console.log('Unable to insert user', err);
+    //     }
+    //     // console.log(JSON.stringify(result.ops, undefined, 2));
+    //     console.log(result.ops[0]._id.getTimestamp());
+    //
+    // });
+
     db.close();
 });
