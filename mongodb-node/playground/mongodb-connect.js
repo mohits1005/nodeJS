0c843fb07f2441f1c3e7a82207ca277fbd53c7a1
mohit sharma <mohit.shar1005@gmail.com>
2018-09-19 17:39:35 +0530

    Mongodb object ID

--- mongodb-node/playground/mongodb-connect.js
+++ mongodb-node/playground/mongodb-connect.js
@@ -1,5 +1,7 @@
-const MongoClient = require('mongodb').MongoClient;
-
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+// var obj = new ObjectID();
+// console.log(obj);
 MongoClient.connect('mongodb://localhost:27017/TodoApp',{ useNewUrlParser: true }, (err, db) => {
     if(err){
         return console.log('Unable to connect to MongoDB server');
