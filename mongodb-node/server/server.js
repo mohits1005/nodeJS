207b0218e154e7ae9a7e5053bd95ceaa831af861
mohit sharma <mohit.shar1005@gmail.com>
2018-09-24 13:17:04 +0530

    mongoose validators data types and defaults

--- mongodb-node/server/server.js
+++ mongodb-node/server/server.js
@@ -5,18 +5,23 @@ mongoose.connect('mongodb://localhost:27017/TodoApp',{ useNewUrlParser: true });
 
 var Todo = mongoose.model('Todo',{
     text: {
-        type: String
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     },
     completedAt: {
-        type: Number
+        type: Number,
+        default: null
     }
 });
 var date = new Date();
 var newTodo = new Todo({
-    text: 'Cut Vegetables',
+    text: '   Edit this video        ',
     completed: true,
     completedAt: date.getTime()
 });
