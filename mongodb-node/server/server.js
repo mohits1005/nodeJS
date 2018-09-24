f3bafe1d9d0caa6924415589e71be62b4e57f5dd
mohit sharma <mohit.shar1005@gmail.com>
2018-09-24 16:22:15 +0530

    adding post routes - saving todos

--- mongodb-node/server/server.js
+++ mongodb-node/server/server.js
@@ -1,52 +1,27 @@
-var mongoose = require('mongoose');
-mongoose.promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp',{ useNewUrlParser: true });
+var express = require('express')
+var bodyParser = require('body-parser')
 
-var Todo = mongoose.model('Todo',{
-    text: {
-        type: String,
-        required: true,
-        minlength: 1,
-        trim: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    },
-    completedAt: {
-        type: Number,
-        default: null
-    }
-});
-var date = new Date();
-var newTodo = new Todo({
-    text: '   Edit this video        ',
-    completed: true,
-    completedAt: date.getTime()
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+//creating todos
+app.post('/todos', (req, res) => {
+    // console.log(req.body)
+    var todo = new Todo({
+        text: req.body.text
+    });
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    });
 });
 
-// newTodo.save().then((doc) => {
-//     console.log('Saved todo',doc);
-// }, (e) => {
-//     console.log('Unable to save todo');
-// });
-
-// User
-// email - require it - trim it - set type - set minimum length of 1
-
-var User = mongoose.model('User',{
-    email: {
-        type: String,
-        required: true,
-        minlength: 1,
-        trim: true
-    }
-});
-var newUser = new User({
-    email: 'mohit@chalkstreet.com'
-});
-newUser.save().then((user) => {
-    console.log('Saved User',user);
-}, (e) => {
-    console.log('Unable to save user');
+app.listen(3000, () => {
+    console.log('Started on port 3000');
 });
