73b718ee008ecff72f4388994ffb50d8422ff3ae
mohit sharma <mohit.shar1005@gmail.com>
2018-09-25 18:29:20 +0530

    adding route to find note by Id

--- mongodb-node/server/server.js
+++ mongodb-node/server/server.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser')
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
-
+const {ObjectID} = require('mongodb');
 var app = express();
 
 app.use(bodyParser.json());
@@ -29,6 +29,21 @@ app.get('/todos', (req, res) => {
     },(e) => {
         res.status(400).send(e);
     });
+});
+
+//fetch specific todo
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)){
+        res.status(404).send();
+    }
+    Todo.findById(id).then((todo) => {
+        if(!todo)
+        {
+            res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e) => res.status(400).send( ));
 });
 
 app.listen(3000, () => {
