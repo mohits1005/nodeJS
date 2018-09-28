bec0b73386173af3170d855893a1e6dacb790fe0
mohit sharma <mohit.shar1005@gmail.com>
2018-09-28 12:21:15 +0530

    adding middleware and checking auth token

--- jwt/server/server.js
+++ jwt/server/server.js
@@ -7,6 +7,7 @@ const bodyParser = require('body-parser')
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
+var { authenticate } = require('./middleware/authenticate');
 const { ObjectID } = require('mongodb');
 var app = express();
 const port = process.env.PORT;
@@ -100,8 +101,12 @@ app.post('/users', (req, res) => {
     }).then((token) => {
         res.header('x-auth', token).send(user);
     }).catch((e) => {
-        res.send(400).send(e);
+        res.status(400).send(e);
     });
+});
+
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
 });
 
 module.exports = {
