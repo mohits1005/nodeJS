06246984d25e5fdcfa298ea1da5d9ccb60836662
mohit sharma <mohit.shar1005@gmail.com>
2018-10-30 01:29:38 +0530

    Add newMessage and createMessage events

---
+++ socket-io/public/js/index.js
@@ -0,0 +1,27 @@
+var socket = io();
+
+socket.on('connect', function() {
+    console.log('Connected to server');
+
+    // socket.emit('createEmail', {
+    //     to: 'aku@example.com',
+    //     text: 'Hey. This is Moh.'
+    // });
+
+    socket.emit('createMessage', {
+        from: 'moh@example.com',
+        text: 'This is a sample message'
+    });
+});
+
+socket.on('disconnect', function() {
+    console.log('Disconnected from server');
+});
+
+// socket.on('newEmail', function(email) {
+//     console.log('New email', email);
+// });
+
+socket.on('newMessage', function(message) {
+    console.log('newMessage', message);
+});
